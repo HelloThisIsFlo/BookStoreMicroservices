0e3177f7611c990e8c503e7b76c0fee064d81bf7
Florian Kempenich <shockn745@gmail.com>
2016-12-10 18:10:45 +0000

    Add DB initialisation for Reviews

--- reviews/src/main/java/com/shockn745/ReviewsApplication.java
+++ reviews/src/main/java/com/shockn745/ReviewsApplication.java
@@ -1,7 +1,8 @@
 package com.shockn745;
 
-import com.shockn745.domain.model.review.ReviewRepository;
+import com.shockn745.domain.model.review.*;
 import com.shockn745.data.TempFakeRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +18,20 @@ public class ReviewsApplication {
     @Bean
     public ReviewRepository getTempRepo() {
 	    return new TempFakeRepo();
+    }
+
+    @Bean
+	public CommandLineRunner fillWithFakeReview(ReviewRepository reviewRepository) {
+		return args -> {
+            reviewRepository.save(makeReview("3", "Georges", 12));
+            reviewRepository.save(makeReview("3", "Patrick", 23));
+            reviewRepository.save(makeReview("1", "Georges", 77));
+            reviewRepository.save(makeReview("1", "Mark", 89));
+            reviewRepository.save(makeReview("1", "Francis", 99));
+		};
+	}
+
+	private Review makeReview(String bookId, String username, int rating) {
+        return new Review(new ReviewId("WILL BE OVERRIDEN"), new BookId(bookId), new User(username), new Rating(rating));
     }
 }
