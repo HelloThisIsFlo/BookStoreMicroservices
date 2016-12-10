992edfc0a2653347723a2e37a8637b45131d5747
Florian Kempenich <shockn745@gmail.com>
2016-12-10 17:15:01 +0000

    Finish implementation with Hibernate

--- books/src/main/java/com/shockn745/data/hibernate/HibernateBookRepository.java
+++ books/src/main/java/com/shockn745/data/hibernate/HibernateBookRepository.java
@@ -1,5 +1,6 @@
 package com.shockn745.data.hibernate;
 
+import com.shockn745.data.hibernate.model.BookJpaEntity;
 import com.shockn745.domain.model.book.Book;
 import com.shockn745.domain.model.book.BookId;
 import com.shockn745.domain.model.book.BookRepository;
@@ -20,24 +21,45 @@ import java.util.List;
 public class HibernateBookRepository implements BookRepository {
 
     private final HibernateCrudBookRepository crudBookRepository;
+    private final JpaMapper jpaMapper;
 
     @Autowired
-    public HibernateBookRepository(HibernateCrudBookRepository crudBookRepository) {
+    public HibernateBookRepository(HibernateCrudBookRepository crudBookRepository, JpaMapper jpaMapper) {
         this.crudBookRepository = crudBookRepository;
+        this.jpaMapper = jpaMapper;
     }
 
     @Override
     public void save(Book book) {
+        BookJpaEntity bookJpaEntity = jpaMapper.map(book);
+        crudBookRepository.save(bookJpaEntity);
 
+        Book saved = jpaMapper.map(bookJpaEntity);
+
+        // Set id
+        book.setId(saved.id());
     }
 
     @Override
     public Book findById(BookId bookId) {
-        return null;
+        BookJpaEntity jpaEntity = crudBookRepository.findOne(parseId(bookId));
+        return jpaMapper.map(jpaEntity);
+    }
+
+    private long parseId(BookId bookId) {
+        return Long.parseLong(bookId.idString());
     }
 
     @Override
     public List<Book> findAll() {
-        return new ArrayList<>();
+        Iterable<BookJpaEntity> allJpaEntities = crudBookRepository.findAll();
+
+        List<Book> result = new ArrayList<>();
+        for (BookJpaEntity jpaEntity : allJpaEntities) {
+            Book book = jpaMapper.map(jpaEntity);
+            result.add(book);
+        }
+
+        return result;
     }
 }
